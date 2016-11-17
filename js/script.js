2fcb036e5935d60362528772b57364e88570ec38
SovaSovitsa <sovasovitsa@gmail.com>
2016-11-17 20:36:14 +0200

    исправление к стилям ссылок сортировки на внутренней

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ var link = document.querySelector(".search-btn");
       
 var popup = document.querySelector(".search-container");
 
-
 link.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.add("search-modal");
